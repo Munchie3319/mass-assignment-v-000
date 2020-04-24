bcaf83ef4233490439a3992d6f58b997f1558869
rosa lee <rosalee0116@gmail.com>
2020-04-24 21:25:49 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,6 +4,6 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
-    attributes.each_char { |key, value| self.send(("#{key}="), value) }
+    attributes.each { |key, value| self.send(("#{key}="), value)}
   end
 end
